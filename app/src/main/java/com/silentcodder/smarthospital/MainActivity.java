917a0d3ee04c7b6d1a19f015e8d3a1c8c838a4d8
silent-codder <ajinkyashinde9799@gmail.com>
2020-11-11 10:32:02 +0530

    add bottom nav

--- app/src/main/java/com/silentcodder/smarthospital/MainActivity.java
+++ app/src/main/java/com/silentcodder/smarthospital/MainActivity.java
@@ -1,14 +1,54 @@
 package com.silentcodder.smarthospital;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.silentcodder.smarthospital.Fragments.HistoryFragment;
+import com.silentcodder.smarthospital.Fragments.HomeFragment;
+import com.silentcodder.smarthospital.Fragments.ProfileFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    Fragment selectFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.nav_home :
+                        selectFragment = new HomeFragment();
+                        break;
+                    case R.id.nav_history :
+                        selectFragment = new HistoryFragment();
+                        break;
+                    case R.id.nav_profile :
+                        selectFragment = new ProfileFragment();
+                        break;
+                }
+
+                if (selectFragment != null)
+                {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectFragment).commit();
+                }
+
+                return true;
+            }
+
+        });
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
     }
 }
